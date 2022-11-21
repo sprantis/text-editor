4f8964fc24ecd118aa308db4ff0f7e55107da493
Sarah Prantis <sprantis@gmail.com>
2022-11-21 18:40:09 -0500

    removed comments

--- server/server.js
+++ server/server.js
@@ -1,21 +1,3 @@
-// GIVEN a text editor web application
-
-
-// WHEN I use next-gen JavaScript in my application
-// THEN I find that the text editor still functions in the browser without errors
-
-// WHEN I enter content and subsequently click off of the DOM window
-// THEN I find that the content in the text editor has been saved with IndexedDB
-
-// WHEN I load my web application
-// THEN I should have a registered service worker using workbox
-
-// WHEN I register a service worker
-// THEN I should have my static assets pre cached upon loading along with subsequent pages and static assets
-
-// WHEN I deploy to Heroku
-// THEN I should have proper build scripts for a webpack application
-
 const express = require('express');
 
 const app = express();
